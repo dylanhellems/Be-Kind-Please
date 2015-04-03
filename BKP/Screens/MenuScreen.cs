ecc78c00fc3abb89ed7dda938d0d7505b45d7643
Dylan Hellems <dylanhellems@gmail.com>
2015-04-02 20:58:43 -0400

    Added custom background

--- BKP/Screens/MenuScreen.cs
+++ BKP/Screens/MenuScreen.cs
@@ -225,9 +225,9 @@ namespace BKP
             float transitionOffset = (float)Math.Pow(TransitionPosition, 2);
 
             // Draw the menu title centered on the screen
-            Vector2 titlePosition = new Vector2(graphics.Viewport.Width / 2, 80);
+            Vector2 titlePosition = new Vector2(graphics.Viewport.Width / 2, 100);
             Vector2 titleOrigin = font.MeasureString(menuTitle) / 2;
-            Color titleColor = new Color(192, 192, 192) * TransitionAlpha;
+            Color titleColor = Color.White * TransitionAlpha;
             float titleScale = 1.25f;
 
             titlePosition.Y -= transitionOffset * 100;
