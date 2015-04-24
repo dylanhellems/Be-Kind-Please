8cfa494cc749af61501f1af05672e58bdcfdcfce
Quentin Moore <qrm6ff@virginia.edu>
2015-04-23 20:16:12 -0400

    Updated WIndow Name
    from Platformer to BKP

--- BKP/Properties/AssemblyInfo.cs
+++ BKP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Platformer")]
-[assembly: AssemblyProduct("Platformer")]
+[assembly: AssemblyTitle("BKP")]
+[assembly: AssemblyProduct("BKP")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
